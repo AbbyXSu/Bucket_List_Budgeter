1f6e8c4eb91c178372dc27cdb4f2c33646fd7802
AbbyXSo <xiaotong.s.abby@gmail.com>
2021-01-27 23:58:31 +0000

    feature/16 in progress linked SQL Server db

--- src/app.py
+++ src/app.py
@@ -1,57 +1,57 @@
-from flask import Flask, redirect, render_template, request, url_for,flash
-from flask_sqlalchemy import SQLAlchemy
-from .user import RegistrationForm, LoginForm
-import sqlalchemy.exc as dberr
-app = Flask(__name__)
-app.config['SECRET_KEY']='814b86ddf016bb20485f4ec09344cdbc'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:password@localhost/usersDB"
+# from flask import Flask, redirect, render_template, request, url_for,flash
+# from flask_sqlalchemy import SQLAlchemy
+# from .user import RegistrationForm, LoginForm
+# import sqlalchemy.exc as dberr
+# app = Flask(__name__)
+# app.config['SECRET_KEY']='814b86ddf016bb20485f4ec09344cdbc'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:password@localhost/usersDB"
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-class users(db.Model):
-    username= db.Column(db.String(50), nullable=False, primary_key=True)
-    first_name= db.Column(db.String(50), nullable=False)  
-    last_name = db.Column(db.String(50), nullable=False)  
+# class users(db.Model):
+#     username= db.Column(db.String(50), nullable=False, primary_key=True)
+#     first_name= db.Column(db.String(50), nullable=False)  
+#     last_name = db.Column(db.String(50), nullable=False)  
 
-db.create_all()
+# db.create_all()
 
-@app.route("/")
-@app.route('/home')
-def home():
-    current_user = request.args.get('users') if request and request.args else None
-    return render_template ('home.html', users=current_user)
+# @app.route("/")
+# @app.route('/home')
+# def home():
+#     current_user = request.args.get('users') if request and request.args else None
+#     return render_template ('home.html', users=current_user)
 
-@app.route('/register', methods=["GET","POST"])
-def register():
-    form = RegistrationForm()
-    new_user: users = None
-    if request.form:
-        try:
-            new_user = users(
-            username = form.username.data,
-            first_name = form.first_name.data,
-            last_name = form.last_name.data)
-            db.session.add(new_user)
-            db.session.commit()
-        except dberr.IntegrityError:
-            flash(f'Account has already been created, try again!')
-            return render_template('register.html', title ='Register',form =form)
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home', users=new_user.first_name))
-    return render_template('register.html', title ='Register',form =form)
+# @app.route('/register', methods=["GET","POST"])
+# def register():
+#     form = RegistrationForm()
+#     new_user: users = None
+#     if request.form:
+#         try:
+#             new_user = users(
+#             username = form.username.data,
+#             first_name = form.first_name.data,
+#             last_name = form.last_name.data)
+#             db.session.add(new_user)
+#             db.session.commit()
+#         except dberr.IntegrityError:
+#             flash(f'Account has already been created, try again!')
+#             return render_template('register.html', title ='Register',form =form)
+#     if form.validate_on_submit():
+#         flash(f'Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('home', users=new_user.first_name))
+#     return render_template('register.html', title ='Register',form =form)
 
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.username.data == 'username':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username', 'danger')
-    return render_template('login.html', title ='Login',form =form)
+# @app.route('/login', methods=["GET","POST"])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         if form.username.data == 'username':
+#             flash('You have been logged in!', 'success')
+#             return redirect(url_for('home'))
+#         else:
+#             flash('Login Unsuccessful. Please check username', 'danger')
+#     return render_template('login.html', title ='Login',form =form)
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True, host='0.0.0.0')
