010097c905e3bfe4edf6d6917b44a75f9409fcd5
AbbyXSo <xiaotong.s.abby@gmail.com>
2021-02-03 01:00:12 +0000

    feat/28 Unittest WIP, web style fixed completed,refactoring completed

--- src/app.py
+++ src/app.py
@@ -17,55 +17,66 @@ from .data_access import TodoList, db, app, Base, Users, TodoItem, Ledger, Budge
 @app.route("/")
 @app.route('/home')
 def home():
-    current_user = request.args.get(
-        'users') if request and request.args else None
+    current_user = get_user(request)
     return render_template('home.html', users=current_user, first_name=request.args.get('first_name'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    new_user: Users = None
+    if not form.validate_on_submit():
+        return render_template('register.html', title='Register', form=form)
     try:
-        if request.form:
-            new_user = Users(
-                Username=form.username.data,
-                first_name=form.first_name.data,
-                last_name=form.last_name.data)
-            db.session.add(new_user)
-            db.session.commit()
-
+        new_user = create_user(form)
     except dberr.IntegrityError:
         flash(f'Account has already been created, try again!')
         return render_template('register.html', title='Register', form=form)
 
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home', first_name=new_user.first_name, users=new_user.Username))
-
-    return render_template('register.html', title='Register', form=form)
+    flash(f'Account created for {form.username.data}!', 'success')
+    return redirect(url_for('home', first_name=new_user.first_name, users=new_user.Username))
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    form = LoginForm()
+    form = LoginForm(request.form)
+    result = validate_login(form)
+    if result and result.first() and result.first().Username == form.username.data:
+        flash('You have been logged in!', 'success')
+        return redirect(url_for('home', users=result.first().Username, first_name=result.first().first_name))
+    else:
+        flash('Login Unsuccessful. Please check username', 'danger')
+        return render_template('login.html', title='Login', form=form)
+
+
+def create_user(form):
+    new_user = Users(
+    Username=form.username.data,
+    first_name=form.first_name.data,
+    last_name=form.last_name.data)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
+
+
+def validate_login(form):
     if form.validate_on_submit():
         result = db.session.query(Users).filter(
-            Users.Username == form.username.data)
+        Users.Username == form.username.data)
         db.session.commit()
-        if result and result.first() and result.first().Username == form.username.data:
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home', users=result.first().Username, first_name=result.first().first_name))
-        else:
-            flash('Login Unsuccessful. Please check username', 'danger')
-            return render_template('login.html', title='Login', form=form)
-    return render_template('login.html', title='Login', form=form)
-#----------------------------------------------------------Bucket Items---------------------------------------------------------------------------------------#
+        return result
+
+def get_user(request):
+    current_user = request.args.get(
+        'users') if request and request.args else None
+    return current_user
+
+
+#----------------------------------------------------------Bucket Items_CRUD---------------------------------------------------------------------------------------#
 
 
 @app.route('/bucketList', methods=['GET', 'POST'])
 def bucketList():
-    request_user = request.args.get('users') if request.args else None
+    request_user = get_user(request)
     if not request_user:
         flash('You do not have an account yet!, Create one!')
         return redirect(url_for('home'))
@@ -74,19 +85,18 @@ def bucketList():
     posts, todoListId = read_bucket_list(request_user)
 
     if request.method == 'GET':
-        # need a different method here which makes a view. return a render template
         return get_bucket_list(posts, form, user=request_user, list_id=todoListId)
 
     if request.method == 'POST':
+        posts, todoListId = read_bucket_list(request_user)
         list_id = create_bucket_list(request_user)
         item_id = add_todoItem_to_list(list_id, form)
-        return render_template('bucketList.html',  title='My_Bucket_List', posts=posts, form=form, bucketListId=str(list_id), users=request_user)
-
+        return redirect(url_for('bucketList', title='My_Bucket_List', posts=posts, form=form, bucketListId=str(list_id), users=request_user))
 
 @app.route('/bucketList/<id>/item', methods=['GET', 'POST'])
 def create_bucket_item(id):
     form = TodoItemForm(request.form)
-    request_user = request.args.get('users') if request.args else None
+    request_user = get_user (request)
     if not request_user:
         flash('You do not have an account yet!, Create one!')
         return redirect(url_for('home'))
@@ -94,64 +104,74 @@ def create_bucket_item(id):
         if form.validate_on_submit():
             flash(
                 f'Item created for {form.Title.data}! in the Bucket List', 'success')
-            # add the new item
             new_item_id = add_todoItem_to_list(id, form)
-            # read the list
             posts = read_bucket_list(request_user)
             return redirect(url_for('bucketList', users=request_user))
         else:
-            # return the view for invalid
             return render_template('createItem.html', title='My_Bucket_List', form=form, bucketList_id=str(id), users=request_user)
     if request.method == 'GET':
-        # return create item view which links to the given bucketlistId
         return render_template('createItem.html', title='Create_My_Bucket_Item', bucketList_id=str(id), users=request_user, form=form)
 
 
 @app.route('/bucketList/<id>/item/<itemId>', methods=['GET', 'POST'])
 def bucket_list_item(id, itemId):
-    # if get, return the edit form populated with this item
     if request.method == 'GET':
-        chosenItem = db.session.query(TodoItem).filter_by(
-            Id=itemId, Todo_List_ID=id).first()
-        db.session.commit()
-        form = TodoItemForm()
-        form.Item_priority.data = chosenItem.Todo_items_Order
-        form.Title.data = chosenItem.Title
-        form.Description.data = chosenItem.Description
-        form.Costs.data = chosenItem.Costs
+        chosenItem = chosen_item(id, itemId)
+        form = TodoItemForm(request.form)
+        prepopulated_item(id, itemId,form)
         return render_template('updateItem.html', title=f'update {chosenItem.Title}', form=form, id=id, itemId=itemId)
     if request.method == 'POST':
         form = TodoItemForm(request.form)
-        chosenItem = db.session.query(TodoItem).filter_by(
-            Id=itemId, Todo_List_ID=id).first()
-        chosenItem.Todo_items_Order = form.Item_priority.data
-        chosenItem.Title = form.Title.data
-        chosenItem.Description = form.Description.data
-        chosenItem.Costs = form.Costs.data
-        db.session.commit()
-        user = db.session.query(TodoList).filter_by(
-            Todo_List_ID=id).first().Username
-        db.session.commit()
+        user = get_user_with_TodoListID(id)
+        update_item(id, itemId, form)
         return redirect(url_for('bucketList', users=user))
 
 
 @app.route('/bucketList/<id>/item/<itemId>/delete', methods=['GET'])
 def delete_bucket_list_item(id, itemId):
+    deleting_item(id, itemId)
+    user = get_user_with_TodoListID(id)
+    return redirect(url_for('bucketList', id=id, itemId=itemId, users=user))
 
-    deleteItem = db.session.query(TodoItem).filter_by(
-        Id=itemId, Todo_List_ID=id).first()
+
+#---------------------------------------------------------------------------------(DB_querry_function_only)-----------------------------------------------------------------# 
+def update_item(id, itemId, form):
+    chosenItem = chosen_item(id, itemId,)
+    chosenItem.Todo_items_Order = form.Item_priority.data
+    chosenItem.Title = form.Title.data
+    chosenItem.Description = form.Description.data
+    chosenItem.Costs = form.Costs.data
+    db.session.commit()
+    return None
+
+def prepopulated_item(id, itemId,form):   
+    chosenItem = chosen_item(id, itemId)
+    form.Item_priority.data = chosenItem.Todo_items_Order
+    form.Title.data = chosenItem.Title
+    form.Description.data = chosenItem.Description
+    form.Costs.data = chosenItem.Costs
+    return form.Item_priority.data, form.Title.data, form.Description.data, form.Costs.data
+
+def chosen_item(id, itemId):
+    chosenItem = db.session.query(TodoItem).filter_by(Id=itemId, Todo_List_ID=id).first()
+    db.session.commit()
+    return chosenItem
+
+def deleting_item(id, itemId):
+    deleteItem = db.session.query(TodoItem).filter_by(Id=itemId, Todo_List_ID=id).first()
     db.session.delete(deleteItem)
     db.session.commit()
-    user = db.session.query(TodoList).filter_by(
-        Todo_List_ID=id).first().Username
+    return deleteItem
+    
+
+def get_user_with_TodoListID(id):
+    user = db.session.query(TodoList).filter_by(Todo_List_ID=id).first().Username
     db.session.commit()
-    return redirect(url_for('bucketList', id=id, itemId=itemId, users=user))
+    return user
 
 #------------------------------------------------------------Bucket_List-------------------------------------------------------------------------------------#
 
-
 def read_bucket_list(user=None):
-    # this method should get the bucketlist from db
     posts = db.session.query(TodoList).filter(TodoList.Username == user).all()
     db.session.commit()
     list_todoItms = list(posts[0].todoitem_collection) if posts and len(
@@ -170,7 +190,7 @@ def get_bucket_list(posts, form, user=None, list_id=None):
     else:
         return render_template('bucketList.html', title='My_Bucket_List', posts=posts, form=form, bucketList_id=str(list_id), users=user)
 
-
+#---------------------------------------------------------------------------------(DB_querry_function_only)-----------------------------------------------------------------# 
 def create_bucket_list(user):
     newList = TodoList(Username=user)
     db.session.add(newList)
@@ -197,7 +217,7 @@ def add_todoItem_to_list(listId, form=None):
 
 @app.route('/budgetSummary', methods=['GET', 'POST'])
 def budgetSummary():
-    request_user = request.args.get('users') if request.args else None
+    request_user = get_user (request)
     if not request_user:
         flash('You do not have an account yet!, Create one!')
         return redirect(url_for('home'))
@@ -210,8 +230,13 @@ def budgetSummary():
         return get_budgetLedger(logs, form, user=request_user, budgeter_ID=budgeter_ID, balance=balance, percentage=round(percentage, 2), diff=diff)
 
     if request.method == 'POST':
+        list_todoItms, _ = read_bucket_list(request_user)
+        diff, percentage, _ = performance_calculator(list_todoItms, logs)
         budgeter_ID = create_budgetlog(request_user)
-        return render_template('budgetSummary.html', title='My_Saving Journey', logs=logs, budgeter_ID=str(budgeter_ID), users=request_user)
+        balance = balance_calculator(logs)
+        add_budgetActivity_to_Ledger(budgeter_ID, form)
+        logs, _ = read_budgetLedger(request_user)
+        return render_template('budgetSummary.html', title='My_Saving Journey', logs=logs, budgeter_ID=str(budgeter_ID), balance = balance, users=request_user, diff = diff, percentage=round(percentage, 2))
 
 
 def read_budgetLedger(user=None):
@@ -245,8 +270,7 @@ def create_budgetlog(user):
 
 def add_budgetActivity_to_Ledger(budgeter_ID, form=None):
     if form:
-        newActivity = Ledger(Value_in_GBP=form.Value_in_GBP.data,
-                             Budgeter_ID=budgeter_ID, Action_ID=form.Action_log.data)
+        newActivity = Ledger(Value_in_GBP=form.Value_in_GBP.data,Budgeter_ID=budgeter_ID, Action_ID=form.Action_log.data)
         db.session.add(newActivity)
         db.session.commit()
         return newActivity.event_id
@@ -256,7 +280,7 @@ def add_budgetActivity_to_Ledger(budgeter_ID, form=None):
 @app.route('/budgetSummary/<id>/item', methods=['GET', 'POST'])
 def budgetAction(id):
     form = LedgerForm(request.form)
-    request_user = request.args.get('users') if request.args else None
+    request_user = get_user (request)
     if not request_user:
         flash('You do not have an account yet!, Create one!')
         return redirect(url_for('home'))
@@ -267,7 +291,6 @@ def budgetAction(id):
             logs, _ = read_budgetLedger(request_user)
             balance = balance_calculator(logs)
             update_balance(id, balance)
-
             return redirect(url_for('budgetSummary', users=request_user))
         else:
             return render_template('budgetAction.html', title='Add_to_your_Savings', form=form, budgeter_ID=id, users=request_user)
@@ -295,7 +318,7 @@ def format_action_type(logs, user=None):  # pytest done
     return logs
 
 
-def balance_calculator(logs):
+def balance_calculator(logs):         # pytest done
     action_operator_converter = {
         1: add,
         2: sub,
@@ -312,7 +335,7 @@ def balance_calculator(logs):
     return balance
 
 
-def performance_calculator(list_todoItms, logs):
+def performance_calculator(list_todoItms, logs):   # pytest done
     total_costs = 0.00
     for item in list_todoItms:
         total_costs += item.Costs
@@ -326,8 +349,7 @@ def performance_calculator(list_todoItms, logs):
 
 @app.route('/myAccount')
 def my_account():
-    request_user = request.args.get(
-        'users') if request and request.args else None
+    request_user = get_user (request)
     list_todoItms, _ = read_bucket_list(request_user)
     logs, _ = read_budgetLedger(request_user)
     numberofBucketItems = number_of_bucketItems(request_user)
@@ -338,8 +360,7 @@ def my_account():
 
 def number_of_bucketItems(request_user):
     _, todoListId = read_bucket_list(request_user)
-    numberofBucketItems = db.session.query(
-        TodoItem).filter_by(Todo_List_ID=todoListId).count()
+    numberofBucketItems = db.session.query(TodoItem).filter_by(Todo_List_ID=todoListId).count()
     return numberofBucketItems
 
 
