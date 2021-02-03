50503a9c13b28b5d7cb59ce1557194938945bc2b
AbbyXSo <xiaotong.s.abby@gmail.com>
2021-02-03 01:38:43 +0000

    feat: refactoring done, unit tests in progress.

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ from sqlalchemy.orm import query, sessionmaker
 from werkzeug.utils import html
 from .user import RegistrationForm, LoginForm, TodoItemForm, TodoListForm, LedgerForm
 import sqlalchemy.exc as dberr
-from .data_access import TodoList, db, app, Base, Users, TodoItem, Ledger, BudgetSummary
+from .data_access import *
+
 
 #---------------------------------------------------------homepage/login/register----------------------------------------------------------------------------------------#
 
@@ -47,28 +48,6 @@ def login():
         flash('Login Unsuccessful. Please check username', 'danger')
         return render_template('login.html', title='Login', form=form)
 
-
-def create_user(form):
-    new_user = Users(
-    Username=form.username.data,
-    first_name=form.first_name.data,
-    last_name=form.last_name.data)
-    db.session.add(new_user)
-    db.session.commit()
-    return new_user
-
-
-def validate_login(form):
-    if form.validate_on_submit():
-        result = db.session.query(Users).filter(
-        Users.Username == form.username.data)
-        db.session.commit()
-        return result
-
-def get_user(request):
-    current_user = request.args.get(
-        'users') if request and request.args else None
-    return current_user
 
 
 #----------------------------------------------------------Bucket Items_CRUD---------------------------------------------------------------------------------------#
@@ -134,51 +113,9 @@ def delete_bucket_list_item(id, itemId):
     return redirect(url_for('bucketList', id=id, itemId=itemId, users=user))
 
 
-#---------------------------------------------------------------------------------(DB_querry_function_only)-----------------------------------------------------------------# 
-def update_item(id, itemId, form):
-    chosenItem = chosen_item(id, itemId,)
-    chosenItem.Todo_items_Order = form.Item_priority.data
-    chosenItem.Title = form.Title.data
-    chosenItem.Description = form.Description.data
-    chosenItem.Costs = form.Costs.data
-    db.session.commit()
-    return None
 
-def prepopulated_item(id, itemId,form):   
-    chosenItem = chosen_item(id, itemId)
-    form.Item_priority.data = chosenItem.Todo_items_Order
-    form.Title.data = chosenItem.Title
-    form.Description.data = chosenItem.Description
-    form.Costs.data = chosenItem.Costs
-    return form.Item_priority.data, form.Title.data, form.Description.data, form.Costs.data
-
-def chosen_item(id, itemId):
-    chosenItem = db.session.query(TodoItem).filter_by(Id=itemId, Todo_List_ID=id).first()
-    db.session.commit()
-    return chosenItem
-
-def deleting_item(id, itemId):
-    deleteItem = db.session.query(TodoItem).filter_by(Id=itemId, Todo_List_ID=id).first()
-    db.session.delete(deleteItem)
-    db.session.commit()
-    return deleteItem
-    
-
-def get_user_with_TodoListID(id):
-    user = db.session.query(TodoList).filter_by(Todo_List_ID=id).first().Username
-    db.session.commit()
-    return user
 
 #------------------------------------------------------------Bucket_List-------------------------------------------------------------------------------------#
-
-def read_bucket_list(user=None):
-    posts = db.session.query(TodoList).filter(TodoList.Username == user).all()
-    db.session.commit()
-    list_todoItms = list(posts[0].todoitem_collection) if posts and len(
-        posts) > 0 else []
-    todoListId = posts[0].Todo_List_ID if posts != [] else None
-    return list_todoItms, todoListId
-
 
 def get_bucket_list(posts, form, user=None, list_id=None):
     if not posts:
@@ -190,27 +127,6 @@ def get_bucket_list(posts, form, user=None, list_id=None):
     else:
         return render_template('bucketList.html', title='My_Bucket_List', posts=posts, form=form, bucketList_id=str(list_id), users=user)
 
-#---------------------------------------------------------------------------------(DB_querry_function_only)-----------------------------------------------------------------# 
-def create_bucket_list(user):
-    newList = TodoList(Username=user)
-    db.session.add(newList)
-    db.session.commit()
-    return newList.Todo_List_ID
-
-
-def add_todoItem_to_list(listId, form=None):
-    # code to add a todoItem
-    if form:
-        new_item = TodoItem(
-            Todo_items_Order=form.Item_priority.data,
-            Todo_List_ID=listId,
-            Title=form.Title.data,
-            Description=form.Description.data,
-            Costs=form.Costs.data)
-        db.session.add(new_item)
-        db.session.commit()
-        return new_item.Id
-    return None
 
 #-------------------------------------------------------------Budget_Summary/Ledger------------------------------------------------------------------------------------#
 
@@ -239,14 +155,7 @@ def budgetSummary():
         return render_template('budgetSummary.html', title='My_Saving Journey', logs=logs, budgeter_ID=str(budgeter_ID), balance = balance, users=request_user, diff = diff, percentage=round(percentage, 2))
 
 
-def read_budgetLedger(user=None):
-    logs = db.session.query(BudgetSummary).filter(
-        BudgetSummary.Username == user).all()
-    db.session.commit()
-    cashflow = list(
-        logs[0].ledger_collection if logs and len(logs) > 0 else [])
-    budgeter_ID = logs[0].Budgeter_Id if logs != [] else None
-    return cashflow, budgeter_ID
+
 
 
 def get_budgetLedger(logs, form, user=None, budgeter_ID=None, balance=0.00, percentage=None, diff=None):
@@ -259,22 +168,6 @@ def get_budgetLedger(logs, form, user=None, budgeter_ID=None, balance=0.00, perc
             return redirect(url_for('budgetAction', id=budgeter_ID, users=user))
     else:
         return render_template('budgetSummary.html', title='My_Savings_journey', logs=logs, form=form, budgeter_ID=budgeter_ID, users=user, balance=balance, percentage=percentage, diff=diff)
-
-
-def create_budgetlog(user):
-    newlog = BudgetSummary(Username=user)
-    db.session.add(newlog)
-    db.session.commit()
-    return newlog.Budgeter_Id
-
-
-def add_budgetActivity_to_Ledger(budgeter_ID, form=None):
-    if form:
-        newActivity = Ledger(Value_in_GBP=form.Value_in_GBP.data,Budgeter_ID=budgeter_ID, Action_ID=form.Action_log.data)
-        db.session.add(newActivity)
-        db.session.commit()
-        return newActivity.event_id
-    return None
 
 
 @app.route('/budgetSummary/<id>/item', methods=['GET', 'POST'])
@@ -299,11 +192,6 @@ def budgetAction(id):
         return render_template('budgetAction.html', title='Add_to_your_Savings', form=form, budgeter_ID=id, users=request_user)
 
 
-def update_balance(id, balance):
-    to_update = db.session.query(
-        BudgetSummary).filter_by(Budgeter_Id=id).first()
-    to_update.Balance = balance
-    db.session.commit()
 
 
 def format_action_type(logs, user=None):  # pytest done
@@ -356,12 +244,6 @@ def my_account():
     balance = balance_calculator(logs)
     diff, percentage, total_costs = performance_calculator(list_todoItms, logs)
     return render_template('myAccount.html', users=request_user, balance=balance, diff=diff, percentage=round(percentage, 2), numberofBucketItems=numberofBucketItems, total_costs=total_costs, logs=logs)
-
-
-def number_of_bucketItems(request_user):
-    _, todoListId = read_bucket_list(request_user)
-    numberofBucketItems = db.session.query(TodoItem).filter_by(Todo_List_ID=todoListId).count()
-    return numberofBucketItems
 
 
 if __name__ == '__main__':
--- src/data_access.py
+++ src/data_access.py
@@ -20,5 +20,141 @@ TodoList = Base.classes.TodoList
 BudgetSummary = Base.classes.BudgetSummary
 
 
+def create_user(form):
+    new_user = Users(
+        Username=form.username.data,
+        first_name=form.first_name.data,
+        last_name=form.last_name.data)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
+
+
+def validate_login(form):
+    if form.validate_on_submit():
+        result = db.session.query(Users).filter(
+            Users.Username == form.username.data)
+        db.session.commit()
+        return result
+
+
+def get_user(request):
+    current_user = request.args.get(
+        'users') if request and request.args else None
+    return current_user
+
+
+def update_item(id, itemId, form):
+    chosenItem = chosen_item(id, itemId,)
+    chosenItem.Todo_items_Order = form.Item_priority.data
+    chosenItem.Title = form.Title.data
+    chosenItem.Description = form.Description.data
+    chosenItem.Costs = form.Costs.data
+    db.session.commit()
+    return None
+
+
+def prepopulated_item(id, itemId, form):
+    chosenItem = chosen_item(id, itemId)
+    form.Item_priority.data = chosenItem.Todo_items_Order
+    form.Title.data = chosenItem.Title
+    form.Description.data = chosenItem.Description
+    form.Costs.data = chosenItem.Costs
+    return form.Item_priority.data, form.Title.data, form.Description.data, form.Costs.data
+
+
+def chosen_item(id, itemId):
+    chosenItem = db.session.query(TodoItem).filter_by(
+        Id=itemId, Todo_List_ID=id).first()
+    db.session.commit()
+    return chosenItem
+
+
+def deleting_item(id, itemId):
+    deleteItem = db.session.query(TodoItem).filter_by(
+        Id=itemId, Todo_List_ID=id).first()
+    db.session.delete(deleteItem)
+    db.session.commit()
+    return deleteItem
+
+
+def get_user_with_TodoListID(id):
+    user = db.session.query(TodoList).filter_by(
+        Todo_List_ID=id).first().Username
+    db.session.commit()
+    return user
+
+
+def read_bucket_list(user=None):
+    posts = db.session.query(TodoList).filter(TodoList.Username == user).all()
+    db.session.commit()
+    list_todoItms = list(posts[0].todoitem_collection) if posts and len(
+        posts) > 0 else []
+    todoListId = posts[0].Todo_List_ID if posts != [] else None
+    return list_todoItms, todoListId
+
+
+def create_bucket_list(user):
+    newList = TodoList(Username=user)
+    db.session.add(newList)
+    db.session.commit()
+    return newList.Todo_List_ID
+
+
+def add_todoItem_to_list(listId, form=None):
+    if form:
+        new_item = TodoItem(
+            Todo_items_Order=form.Item_priority.data,
+            Todo_List_ID=listId,
+            Title=form.Title.data,
+            Description=form.Description.data,
+            Costs=form.Costs.data)
+        db.session.add(new_item)
+        db.session.commit()
+        return new_item.Id
+    return None
+
+
+def create_budgetlog(user):
+    newlog = BudgetSummary(Username=user)
+    db.session.add(newlog)
+    db.session.commit()
+    return newlog.Budgeter_Id
+
+
+def add_budgetActivity_to_Ledger(budgeter_ID, form=None):
+    if form:
+        newActivity = Ledger(Value_in_GBP=form.Value_in_GBP.data,
+                             Budgeter_ID=budgeter_ID, Action_ID=form.Action_log.data)
+        db.session.add(newActivity)
+        db.session.commit()
+        return newActivity.event_id
+    return None
+
+
+def read_budgetLedger(user=None):
+    logs = db.session.query(BudgetSummary).filter(
+        BudgetSummary.Username == user).all()
+    db.session.commit()
+    cashflow = list(
+        logs[0].ledger_collection if logs and len(logs) > 0 else [])
+    budgeter_ID = logs[0].Budgeter_Id if logs != [] else None
+    return cashflow, budgeter_ID
+
+
+def update_balance(id, balance):
+    to_update = db.session.query(
+        BudgetSummary).filter_by(Budgeter_Id=id).first()
+    to_update.Balance = balance
+    db.session.commit()
+
+
+def number_of_bucketItems(request_user):
+    _, todoListId = read_bucket_list(request_user)
+    numberofBucketItems = db.session.query(
+        TodoItem).filter_by(Todo_List_ID=todoListId).count()
+    return numberofBucketItems
+
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
